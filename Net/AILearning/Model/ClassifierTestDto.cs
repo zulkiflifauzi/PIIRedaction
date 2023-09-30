5529987b6346574f385310878a467b876b5f9c4c
zulkifl fauzi <zulkifli.fauzi@polyrific.com>
2023-10-01 00:48:20 +0700

    Update Classifier

---
+++ Net/AILearning/Model/ClassifierTestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AILearning.Model
+{
+    public class ClassifierTestDto
+    {
+        public string FileName { get; set; }
+        public string Sentence { get; set; }
+        public string TestSentence { get; set; }
+    }
+}
